aa2060025d906b374c266c7687303423e51010d9
Jinzo03 <iyeddogaz@gmail.com>
2026-09-13 21:36:46 +0100

    Add OpenID Connect provider and IAM roles for GitHub Actions integration

---
+++ terraform/oidc.tf
@@ -0,0 +1,86 @@
+variable "github_repo" {
+  description = "GitHub repository in format 'owner/repo-name'"
+  type        = string
+  default     = "Jinzo03/devops-fastapi-aws" # Replace with your GitHub username/repo
+}
+
+# 1. OpenID Connect Provider for GitHub
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1", "1c58a218dc7d179696353f5844db10f0a43972a0"]
+}
+
+# 2. IAM Role for GitHub Actions Runner
+resource "aws_iam_role" "github_actions" {
+  name = "${var.app_name}-github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_repo}:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# 3. Permissions for ECR and ECS deployment
+resource "aws_iam_role_policy" "github_actions_policy" {
+  name = "${var.app_name}-github-actions-policy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "ecs:DescribeServices",
+          "ecs:DescribeTaskDefinition",
+          "ecs:RegisterTaskDefinition",
+          "ecs:UpdateService"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = aws_iam_role.ecs_execution_role.arn
+      }
+    ]
+  })
+}
+
+output "github_actions_role_arn" {
+  description = "ARN of IAM Role for GitHub Actions to assume"
+  value       = aws_iam_role.github_actions.arn
+}
