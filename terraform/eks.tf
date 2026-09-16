985f6e6086c3482b7ef0bc91ef08823ac0aff442
Jinzo03 <iyeddogaz@gmail.com>
2026-09-16 15:53:47 +0100

    feat: add EKS cluster and related resources with Helm chart for FastAPI application

---
+++ terraform/eks.tf
@@ -0,0 +1,82 @@
+# IAM Role for EKS Control Plane
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.app_name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+# EKS Cluster Definition
+resource "aws_eks_cluster" "main" {
+  name     = "${var.app_name}-eks"
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
+}
+
+# IAM Role for EKS Managed Worker Nodes
+resource "aws_iam_role" "eks_nodes" {
+  name = "${var.app_name}-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ecr_read" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodes.name
+}
+
+# Managed Node Group
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.app_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = [aws_subnet.public_1.id, aws_subnet.public_2.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  # Changed from t3.medium to t3.micro for Free Tier eligibility
+  instance_types = ["t3.micro"]
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_worker_node,
+    aws_iam_role_policy_attachment.eks_cni,
+    aws_iam_role_policy_attachment.eks_ecr_read,
+  ]
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,12 @@ output "alb_dns_name" {
   description = "Public URL of Application Load Balancer"
   value       = aws_lb.main.dns_name
 }
+output "eks_cluster_name" {
+  description = "Name of the provisioned EKS Cluster"
+  value       = aws_eks_cluster.main.name
+}
+
+output "eks_cluster_endpoint" {
+  description = "Kubernetes API server endpoint"
+  value       = aws_eks_cluster.main.endpoint
+}
