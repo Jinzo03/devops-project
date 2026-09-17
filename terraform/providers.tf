1aee20cc8ffbab4a99e00ecbcb6d75cfc80a5f36
Jinzo03 <iyeddogaz@gmail.com>
2026-09-17 21:24:39 +0100

    feat: update Helm provider configuration to use exec block for EKS token retrieval

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -51,6 +51,12 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.main.token
+
+    # Replace the "token" attribute with this exec block
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
+      command     = "aws"
+    }
   }
 }
